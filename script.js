7b402b25af9c9bb231bdd8591495747b6fa24a9c
JopraZ <louisdemol9@gmail.com>
2025-11-12 13:32:02 +0100

    step 2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const display = document
+const display = document.getElementById("display");
+const button = document.querySelectorAll ("btn");
+
+let currentInput = "0";
+let previousInput = "";
+let operator = null;
+let shouldResetDisplay = false;
+
+display.value = currentInput;
+
+button.forEach(button => {
+    button.addEventListener("click" ,() => {
+        const value = button.getAttribute(`data-value`);
+        handleButtonClick(value);
+    });
+});
