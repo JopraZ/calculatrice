c17b62417c80c167435aa414623a08f92d72842a
JopraZ <louisdemol9@gmail.com>
2025-11-13 07:18:10 +0100

    step 4

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ button.forEach(button => {
         handleButtonClick(value);
     });
 });
+
+function handleButtonClick(value) {
+    console.log('bouton cliqué',value);
+    
+    if (isNumber(value)) {
+        handleNumber(value);
+    } else if (isOperator(value)) {
+        handleOperator(value);
+    } else if (value === "=") {
+        handleEqual();
+    } else if (value === "C") {
+        handleClear();
+    } else if (value === "CE") {
+        handleClearEntry();
+    } else if (value === "backspace") {
+        handleBackspace();
+    } else if (value === ".") {
+        handleDecimal();
+    }
+
+    updateDisplay();
+}
+
+function handleNumber(number) {
+    if (shouldResetDisplay) {
+        currentInput="";
+        shouldResetDisplay = false;
+    }
+    
+    if (currentInput === "0") {
+        currentInput = number;
+    } else {
+        currentInput += number;
+    }
+}
